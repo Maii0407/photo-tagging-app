6bc8e361acd1cf40a519007774ed3381a34e8f60
Akmal Izuddin <izuddin0407@gmail.com>
2022-03-22 10:22:57 +0800

    initial commit

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyCsIecbbPV39JcbDaAZogVs3BZiQiRN5-o",
+  authDomain: "phototag-57b68.firebaseapp.com",
+  projectId: "phototag-57b68",
+  storageBucket: "phototag-57b68.appspot.com",
+  messagingSenderId: "65033794979",
+  appId: "1:65033794979:web:f5062d4459b55709fe9616"
+};
+
+const app = initializeApp( config );
